3528a757dbf38a74b3ba22802946bc4a0175b6dd
swiftfoot <swiftfoot@hotmail.com>
2018-06-09 18:00:00 -0500

    fix(bettertransitions)

--- src/components/SiteDetails/SiteDetails.jsx
+++ src/components/SiteDetails/SiteDetails.jsx
@@ -43,7 +43,7 @@ class SiteDetails extends Component {
         mountOnEnter
         unmountOnExit
         in={isOpen}
-        timeout={1000}
+        timeout={2000}
         classNames="bottom"
       >
         <div className="siteDetails">
