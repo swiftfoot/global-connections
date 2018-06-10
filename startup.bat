ab894cc5fcf741429a717563c1cab204036bdca7
swiftfoot <swiftfoot@hotmail.com>
2018-06-09 22:10:53 -0500

    kiosk

--- startup.bat
+++ startup.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE Chrome Kiosk Mode
 taskkill /f /im explorer.exe
-start "" "c:\Program Files (x86)\Google\Chrome" --chrome-frame --kiosk -incognito /max -fullscreen http://localhost:3000
+start "" "c:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --chrome-frame --kiosk -incognito /max -fullscreen http://localhost:3000
