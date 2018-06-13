7c019ce0f2c20f0915c5879cb73f168124346887
swiftfoot <swiftfoot@hotmail.com>
2018-06-12 20:31:19 -0500

    feat(bluescrim): Add blue scrim background
    add blue scrim background

--- src/components/PullScreen/PullScreen.jsx
+++ src/components/PullScreen/PullScreen.jsx
@@ -21,6 +21,7 @@ const PullScreen = props => {
   if (background.indexOf("mp4")) {
     return (
       <div onClick={onClick}>
+        <div className="videoScrim" />
         <video width="100%" height="100%" autoPlay muted loop>
           <source src={background} />
         </video>
